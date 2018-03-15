40723152e241c5cfe2be44fb3f6c84209a2f8093
snakenest <182860650@qq.com>
2018-03-15 09:24:21 +0800

    warrant20180315

--- warrant/settings.py
+++ warrant/settings.py
@@ -16,10 +16,10 @@ NEWSPIDER_MODULE = 'warrant.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER'
+#USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -52,9 +52,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'warrant.middlewares.WarrantDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'warrant.middlewares.WarrantDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
--- warrant/spiders/warrant.py
+++ warrant/spiders/warrant.py
@@ -5,7 +5,12 @@ import scrapy
 class warrantSpider(scrapy.Spider):
     name = 'warrant'
     #allowed_domains = ['example.com']
-    start_urls = ['http://vip.stock.finance.sina.com.cn/quotes_service/view/hk_warrant_search.php?stock_symbol=01918&publisher=&class=a/']
+    start_urls = ['http://vip.stock.finance.sina.com.cn/quotes_service/view/hk_warrant_search.php?stock_symbol=01918&publisher=&class=a']
+    debugged = False
 
     def parse(self, response):
-        print response.xpath('//div[@class="content"]').extract()
+        if not self.debugged:
+            self.debugged = True
+            from scrapy.shell import inspect_response
+            inspect_response(response, self)
+        print response.xpath('//div[@class = "tbl_wrap"]').extract()
